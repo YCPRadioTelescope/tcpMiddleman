f0456a783a280c6c7f2595d105a24b336d6ba6d6
Lucas Gartrell <lplaudsmith@ycp.edu>
2021-11-07 17:03:21 -0800

    Converted Socket to Websocket for Front end to Middleman Communication

--- middleman-server.js
+++ middleman-server.js
@@ -73,92 +73,79 @@ proxy.on("proxyRes", function(proxyRes, req, res) {
 	enableCors(req, res);
 });
 
-
-https.createServer(options, function(req, res)
-{
-    if (req.method === 'OPTIONS') 
-    {
-        enableCors(req, res);
-        res.writeHead(204);
-        res.end();
-        return;
-    }
-
-    basic.check(req, res) 
-    {
-        enableCors(req, res); 
-        console.log("PROXY: forwarding proxy request to middleman");
-        // forward the authenticated request to the middleman
-        proxy.web(req, res, { target: "http://127.0.0.1:5001", 
-                              secure: true,
-		                      changeOrigin: true });
-
-    }
-}).listen(proxyPort, function() {
-        console.log("proxy server listening at https://127.0.0.1:" + proxyPort + "/");
-});
-
  
+//Receive Command from Front End using WEBSOCKET Server. Requires Websocket, they are different from regular SOCKETS
+//REGULAR SOCKETS ARE NOT BROWSER COMPATIBLE!!!!
+var frontEndCommand = "Something went wrong...";
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 2222 })
+console.log("WebSocket Server Created On Port 2222");
 
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(frontEndCommand) {
+    console.log('FRONT END: %s', frontEndCommand);
+    ws.send('Middleman Recieved Your Message and will forward it to control room.');
 
-///
-/// create the middleman itself
-///
-http.createServer(function(req, res) {
-    // return a success message & code so we know if we passed auth
-    res.statusCode = 200;
-    res.end("MIDDLEMAN: Succesfully connected through proxy server to middleman");
-}
-).listen(middlemanPort, function() 
-{
-    console.log("middleman listening at http://127.0.0.1:" + middlemanPort + "/");
-
-}).on('connection', function(socket)
-{
-    console.log('MIDDLEMAN: A new connection has been established.');
-
-    // Now that a TCP connection has been established, the server can send data to
-    // the client by writing to its socket.
-    // socket.write('Hello, source client.'); <-- commented out, old debugging print
-    
-    // The server can also receive data from the client by reading from its socket.
-    socket.on('data', function(chunk) 
+    // BEGIN ACTING AS CLIENT
+    // fwd data to final server
+    var client = new net.Socket();
+    // the control room is on the same computer, so local host for address
+    // forwardPort should be the port the control room listens on
+    client.connect(forwardPort, '127.0.0.1', function() 
     {
-        console.log(`MIDDLEMAN: Data received from client:\n\n${chunk.toString()}`);
-        console.log('MIDDLEMAN: Finished printing data from client\n');
-
-        // BEGIN ACTING AS CLIENT
-        // fwd data to final server
-        var client = new net.Socket();
-
-        // the control room is on the same computer, so local host for address
-        // forwardPort should be the port the control room listens on
-        client.connect(forwardPort, '127.0.0.1', function() 
-        {
-            console.log('MIDDLEMAN: Connected to final server');
-
-            // TODO: how does the control room interpret the data? Is it better to send plain text as we are here?
-            client.write(chunk.toString());
-        });
-
-        client.on('data', function(data) 
-        {
-            console.log('MIDDLEMAN: Received: ' + data);
-            // destroy the connection each time, we want the user to reauthenticate each time
-            client.destroy(); 
-        });
+        console.log('MIDDLEMAN: Connected to final server');
+        client.write(frontEndCommand);
     });
 
-    // When the client requests to end the TCP connection with the server, the server
-    // ends the connection.
-    socket.on('end', function() 
+    client.on('data', function(data) 
     {
-        console.log('Closing connection with the source client');
+        console.log('CONTROL ROOM: ' + data);
+        // destroy the connection each time, we want the user to reauthenticate each time
+        client.destroy(); 
     });
-    
-    // Don't forget to catch error, for your own sake.
-    socket.on('error', function(err)
-    {
-        console.log(`Error: ${err}`);
+    wss.close();
     });
 });
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ///
+// /// create the middleman itself
+// ///
+// http.createServer(function(req, res) {
+//     // return a success message & code so we know if we passed auth
+//     res.statusCode = 200;
+//     res.end("MIDDLEMAN: Succesfully connected through proxy server to middleman");
+// }
+// ).listen(middlemanPort, function() 
+// {
+//     console.log("middleman listening at http://127.0.0.1:" + middlemanPort + "/");
+
+// }).on('connection', function(socket)
+// {
+//     console.log('MIDDLEMAN: A new connection has been established.');
+
+//     // Now that a TCP connection has been established, the server can send data to
+//     // the client by writing to its socket.
+//     // socket.write('Hello, source client.'); <-- commented out, old debugging print
+    
+//     // The server can also receive data from the client by reading from its socket.
+//     socket.on('data', function(chunk) 
+//     {
+//         console.log(`MIDDLEMAN: Data received from client:\n\n${chunk.toString()}`);
+//         console.log('MIDDLEMAN: Finished printing data from client\n');
+
+
+
+//     // When the client requests to end the TCP connection with the server, the server
+//     // ends the connection.
+//     socket.on('end', function() 
+//     {
+//         console.log('Closing connection with the source client');
+//     });
+    
+//     // Don't forget to catch error, for your own sake.
+//     socket.on('error', function(err)
+//     {
+//         console.log(`Error: ${err}`);
+//     });
+// });
